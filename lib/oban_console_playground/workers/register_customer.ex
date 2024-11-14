6b6b4b092e5970677f7ec007429747fe25f035c1
linqueta <lincolnrodrs@gmail.com>
2024-11-13 23:31:22 -0300

    feat: Improve customer email

--- lib/oban_console_playground/workers/register_customer.ex
+++ lib/oban_console_playground/workers/register_customer.ex
@@ -3,10 +3,10 @@ defmodule ObanConsolePlayground.Workers.RegisterCustomer do
 
   @impl true
   def perform(%Oban.Job{}) do
-    ObanConsolePlayground.Accounts.create_customer(%{
-      name: Faker.Person.name(),
-      email: Faker.Internet.email()
-    })
+    name = Faker.Person.name()
+    email = "#{name |> String.downcase() |> String.replace(" ", ".")}@#{Faker.Internet.domain_name()}"
+
+    ObanConsolePlayground.Accounts.create_customer(%{name: name, email: email})
 
     :ok
   end
